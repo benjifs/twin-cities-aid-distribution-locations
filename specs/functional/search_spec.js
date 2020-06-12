8ee727df0857e1559238365b419e55aed5466d9e
Benji Encalada Mora <benjifs@users.noreply.github.com>
2020-06-12 02:22:23 -0500

    test: lets try again

--- specs/functional/search_spec.js
+++ specs/functional/search_spec.js
@@ -11,27 +11,27 @@ fixture `Search`
   })
 
 test('Search for term', async t => {
-  const listCount = await Selector('.location-list--item').count
-  console.log('listCount', listCount)
+  const list = await Selector('.location-list--item')
+  console.log('listCount', list.count)
 
   // This will have to change if we switch to drawing the map with canvas
   await t
     // Markers on the map should equal the listCount
-    .expect(Selector('.mapboxgl-marker').filterVisible().count).eql(listCount)
+    .expect(Selector('.mapboxgl-marker').filterVisible().count).eql(list.count)
     // Type "test" into the search input
     .typeText(Selector('#search'), 'test')
     // Wait 500ms
     .wait(500)
     // New results should not equal the original count
-    .expect(Selector('.location-list--item').count).notEql(listCount)
-    .expect(Selector('.mapboxgl-marker').filterVisible().count).notEql(listCount)
+    .expect(Selector('.location-list--item').count).notEql(list.count)
+    .expect(Selector('.mapboxgl-marker').filterVisible().count).notEql(list.count)
     // URL should include search parameters
     .expect(getURL()).contains('?search=test')
 })
 
 test('Clear Search', async t => {
-  const listCount = await Selector('.location-list--item').count
-  console.log('listCount', listCount)
+  const list = await Selector('.location-list--item')
+  console.log('listCount', list.count)
 
   // This will have to change if we switch to drawing the map with canvas
   await t
@@ -41,18 +41,18 @@ test('Clear Search', async t => {
     // Wait 1000ms
     .wait(1000)
     // New results should not equal the original count
-    .expect(Selector('.location-list--item').count).notEql(listCount)
+    .expect(Selector('.location-list--item').count).notEql(list.count)
     // Click on the "Clear Search" button
     .click(Selector('#clear-search-btn'))
     // Wait 1000ms
     .wait(1000)
     // List count should equals the original listCount
-    .expect(Selector('.location-list--item').count).eql(listCount)
+    .expect(Selector('.location-list--item').count).eql(list.count)
 })
 
 test('Navigate to page with search parameter', async t => {
-  const listCount = await Selector('.location-list--item').count
-  console.log('listCount', listCount)
+  const list = await Selector('.location-list--item')
+  console.log('listCount', list.count)
   const url = await getURL()
   console.log('url', url)
 
@@ -65,5 +65,5 @@ test('Navigate to page with search parameter', async t => {
     // Search box value should be "test"
     .expect(Selector('#search').value).eql('test')
     // New results should not equal the original count
-    .expect(Selector('.location-list--item').count).notEql(listCount)
+    .expect(Selector('.location-list--item').count).notEql(list.count)
 })
